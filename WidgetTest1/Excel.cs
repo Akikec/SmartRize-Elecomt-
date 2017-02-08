c818b86ceb235501648bea9d7f4795e9d0ba64e5
Akikec <bullyboy131@gmail.com>
2017-02-08 16:53:08 +0300

    Таблица Excel ... Требует корректировки

--- WidgetTest1/Excel.cs
+++ WidgetTest1/Excel.cs
@@ -21,13 +21,21 @@ namespace WidgetTest1
             //Таблица.
             ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook.Sheets[1];
             ObjWorkSheet.Name = "Смартрица";
+            ObjWorkSheet.Cells[1, 1] = "ID";
+            ObjWorkSheet.Cells[1, 2] = "Компания";
+            ObjWorkSheet.Cells[1, 3] = "Приз";
+            ObjWorkSheet.Cells[1, 4] = "Победитель";
+            ObjWorkSheet.Cells[1, 5] = "Дата начала конкурса";
         }
 
-        public void PrintinRow(int column, Winners win)
+        public void PrintinRow(int column, Winners win, bool kl)
         {
+            string giperLink1 = "=ГИПЕРССЫЛКА(\"http://www.elecomt.ru/admin/edit/games/" + Convert.ToString(win.id) + "\";" + Convert.ToString(win.id) + ")";
+            string giperLink2 = "=ГИПЕРССЫЛКА(\"http://www.elecomt.ru/admin/edit/games/_managers/" + Convert.ToString(win.id) + "\";" + Convert.ToString(win.id) + ")";
             //Заполняем значениями.
             //Значения [y - строка,x - столбец]
-            ObjWorkSheet.Cells[column, 1] = win.id;
+            //if (kl) ObjWorkSheet.Cells[column, 1] = giperLink1; //Нужно исправить ошибку
+            //else ObjWorkSheet.Cells[column, 1] = giperLink2;
             ObjWorkSheet.Cells[column, 2] = win.company;
             ObjWorkSheet.Cells[column, 3] = win.prize;
             ObjWorkSheet.Cells[column, 4] = win.winner;
