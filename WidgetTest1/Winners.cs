b1979032ffefcf0f5aa3c1ddc7dc5f49619beeea
Akikec <bullyboy131@gmail.com>
2017-02-08 13:41:05 +0300

    Метод обработки Excel, подключение библиотеки

--- WidgetTest1/Winners.cs
+++ WidgetTest1/Winners.cs
@@ -12,6 +12,8 @@ namespace WidgetTest1
         public string winner;
         public string prize;
 
+        public Winners() { }
+                
         public Winners(int id, string company, string date, string winner, string prize)
         {
             this.id = id;
@@ -33,6 +35,26 @@ namespace WidgetTest1
         public void PrintinRow()
         {
             //заполнение в Excel.
+
+            Microsoft.Office.Interop.Excel.Application ObjExcel = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook ObjWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet;
+            //Книга.
+            ObjWorkBook = ObjExcel.Workbooks.Add(System.Reflection.Missing.Value);
+            //Таблица.
+            ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook.Sheets[1];
+
+
+            //Заполняем значениями.
+            //Значения [y - строка,x - столбец]
+            ObjWorkSheet.Cells[3, 1] = 1;
+            ObjWorkSheet.Cells[3, 2] = 2;
+            ObjWorkSheet.Cells[3, 3] = 3;
+
+
+            //В итоге, делаем созданную эксельку видимой и доступной!
+            ObjExcel.Visible = true;
+            ObjExcel.UserControl = true;
         }
     }
 }
